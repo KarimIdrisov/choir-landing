1ac3036cd93c60e3b7e8acc913088755f5edd912
Karim Idrisov <karim.idrisow2016@gmail.com>
2022-10-04 13:43:29 +1000

    try to merge

--- src/router/router.js
+++ src/router/router.js
@@ -7,7 +7,7 @@ const AppRouter = () => {
     return (
         <BrowserRouter>
             <Routes>
-                <Route path='/' element={<Home />} />
+                <Route path='*' element={<Home />} />
             </Routes>
         </BrowserRouter>
     )
