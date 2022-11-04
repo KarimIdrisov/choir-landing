890c59d6d5b41f9289f0c0b740eacead0e170330
Karim Idrisov <karim.idrisow2016@gmail.com>
2022-11-05 01:24:20 +1000

    add album 2

--- src/photos/PhotoAlbum1.jsx
+++ src/photos/PhotoAlbum1.jsx
@@ -11,6 +11,22 @@ import A5 from '../assets/images/IMG_1573.jpg';
 import A6 from '../assets/images/IMG_1646.jpg';
 import A7 from '../assets/images/IMG_1672.jpg';
 
+import A8 from '../assets/images/1.jpg';
+import A9 from '../assets/images/BA2A7980.jpg';
+import A10 from '../assets/images/BA2A8107.jpg';
+import A11 from '../assets/images/BA2A8223.jpg';
+import A12 from '../assets/images/BA2A8262.jpg';
+import A13 from '../assets/images/BA2A8272.jpg';
+import A14 from '../assets/images/BA2A8396.jpg';
+import A15 from '../assets/images/BA2A8524.jpg';
+import A16 from '../assets/images/IMG_1398.jpg';
+import A17 from '../assets/images/IMG_1400.jpg';
+import A18 from '../assets/images/IMG_1429.jpg';
+import A19 from '../assets/images/IMG_1453.jpg';
+import A20 from '../assets/images/IMG_1485.jpg';
+import A21 from '../assets/images/IMG_1586.jpg';
+import A22 from '../assets/images/IMG_1631.jpg';
+
 import ImageGallery from 'react-image-gallery';
 
 export const PhotoAlbum1 = withLayout(() => {
@@ -46,7 +62,67 @@ export const PhotoAlbum1 = withLayout(() => {
     {
       original: A7,
       thumbnail: A7
-    }
+    },
+    {
+      original: A8,
+      thumbnail: A8
+    },
+    {
+      original: A9,
+      thumbnail: A9
+    },
+    {
+      original: A10,
+      thumbnail: A10
+    },
+    {
+      original: A11,
+      thumbnail: A11
+    },
+    {
+      original: A12,
+      thumbnail: A12
+    },
+    {
+      original: A13,
+      thumbnail: A13
+    },
+    {
+      original: A14,
+      thumbnail: A14
+    },
+    {
+      original: A15,
+      thumbnail: A15
+    },
+    {
+      original: A16,
+      thumbnail: A16
+    },
+    {
+      original: A17,
+      thumbnail: A17
+    },
+    {
+      original: A18,
+      thumbnail: A18
+    },
+    {
+      original: A19,
+      thumbnail: A19
+    },
+    {
+      original: A20,
+      thumbnail: A20
+    },
+    {
+      original: A21,
+      thumbnail: A21
+    },
+    {
+      original: A22,
+      thumbnail: A22
+    },
   ];
 
   const navigate = useNavigate();
--- src/router/router.js
+++ src/router/router.js
@@ -3,6 +3,7 @@ import { BrowserRouter, Route, Routes } from "react-router-dom";
 import { News1 } from "../news/News1";
 import Home from "../pages/Home";
 import { PhotoAlbum1 } from "../photos/PhotoAlbum1";
+import { PhotoAlbum2 } from "../photos/PhotoAlbum2";
 
 const AppRouter = () => {
   return (
@@ -10,6 +11,7 @@ const AppRouter = () => {
       <Routes>
         <Route path="/news1" element={<News1 />} />
         <Route path="/photo-album-1" element={<PhotoAlbum1 />} />
+        <Route path="/photo-album-2" element={<PhotoAlbum2 />} />
         <Route path="*" element={<Home />} />
       </Routes>
     </BrowserRouter>
